d044fb551047c072e493579c12805bcdbe635d7b
Darshan <np03a190140@heraldcollege.edu.np>
2021-05-01 19:38:58 +0545

    add Range selector

---
+++ static/js/range.js
@@ -0,0 +1,21 @@
+var slider = document.getElementById("slider");
+var selector = document.getElementById("selector");
+var SelectValue = document.getElementById("SelectValue");
+
+SelectValue.innerHTML = slider.value;
+
+slider.oninput = function(){
+    SelectValue.innerHTML = this.value;
+    selector.style.left = this.value + "%";
+}
+
+var slider2 = document.getElementById("slider2");
+var selector2 = document.getElementById("selector2");
+var SelectValue2 = document.getElementById("SelectValue2");
+
+SelectValue2.innerHTML = slider2.value;
+
+slider2.oninput = function(){
+    SelectValue2.innerHTML = this.value;
+    selector2.style.left = this.value + "%";
+}
